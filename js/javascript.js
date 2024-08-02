8707c940d6a99cd2b85151f26bd944b6775b6fe6
Vitalii Vynnychenko <vitalii.vynnych65@gmail.com>
2024-08-02 20:42:41 +0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,58 +1,12 @@
-const menu = document.querySelector(".menu");
-const menuMain = menu.querySelector(".menu-main");
-const goBack = menu.querySelector(".go-back");
-const menuTrigger = document.querySelector(".mobile-menu-trigger");
-const closeMenu = menu.querySelector(".mobile-menu-close");
-let subMenu;
-menuMain.addEventListener("click", (e) =>{
-    if(!menu.classList.contains("active")){
-        return;
-    }
-  if(e.target.closest(".menu-item-has-children")){
-       const hasChildren = e.target.closest(".menu-item-has-children");
-     showSubMenu(hasChildren);
-  }
-});
-goBack.addEventListener("click",() =>{
-     hideSubMenu();
-})
-menuTrigger.addEventListener("click",() =>{
-     toggleMenu();
-})
-closeMenu.addEventListener("click",() =>{
-     toggleMenu();
-})
-document.querySelector(".menu-overlay").addEventListener("click",() =>{
-    toggleMenu();
-})
-function toggleMenu(){
-    menu.classList.toggle("active");
-    document.querySelector(".menu-overlay").classList.toggle("active");
-}
-function showSubMenu(hasChildren){
-   subMenu = hasChildren.querySelector(".sub-menu");
-   subMenu.classList.add("active");
-   subMenu.style.animation = "slideLeft 0.5s ease forwards";
-   const menuTitle = hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
-   menu.querySelector(".current-menu-title").innerHTML=menuTitle;
-   menu.querySelector(".mobile-menu-head").classList.add("active");
-}
+// Select the subMenu element
+const subMenu = document.getElementById("subMenu");
 
-function  hideSubMenu(){  
-   subMenu.style.animation = "slideRight 0.5s ease forwards";
-   setTimeout(() =>{
-      subMenu.classList.remove("active");	
-   },300); 
-   menu.querySelector(".current-menu-title").innerHTML="";
-   menu.querySelector(".mobile-menu-head").classList.remove("active");
-}
-
-window.onresize = function(){
-    if(this.innerWidth >991){
-        if(menu.classList.contains("active")){
-            toggleMenu();
-        }
-
+// Function to toggle the menu's visibility
+function toggleMenu() {
+    // Check if subMenu exists before toggling the class
+    if (subMenu) {
+        subMenu.classList.toggle("open-menu");
+    } else {
+        console.error("Element with ID 'subMenu' not found.");
     }
 }
-
